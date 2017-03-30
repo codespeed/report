0212636fc74c4bcef99e5383ea3670758e297abe
codespeed22 <maghanoycm@gmail.com>
2017-03-31 00:09:24 +0800

    updated

--- view.php
+++ view.php
@@ -1,4 +1,9 @@
 <?php
+  header('X-Frame-Options: EPROSESO');
+
+  require('fpdf/fpdf.php');
+
+
    $mlab = new MongoClient("mongodb://eproseso:eproseso@ds059682.mlab.com:59682/eproseso");
    $db = $mlab->eproseso;
    $applications = $db->applications;
@@ -9,7 +14,24 @@
       echo $row["lastname"] . "\n";
    }
 
+   $d = "";
+   $m= "";
+   $y = "";
+
+   if(isset($_GET['d']) && isset($_GET['m']) && isset($_GET['y'])){
+   	$d = $_GET['m'] ."-".$_GET['d']."-".$_GET['y'];
+   }
+
+   if(isset($_GET['m']) && isset($_GET['y'])){
+   	$m = $_GET['m'] ."-".$_GET['d']."-".$_GET['y'];
+   }
+
+   if(isset($_GET['y'])){
+   	$y = $_GET['y'];
+   }
+
   
+
 
 
 ?>
