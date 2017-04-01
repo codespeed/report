c2a72598fdf33a734278ac026faffeb8f7635e6e
codespeed22 <maghanoycm@gmail.com>
2017-04-01 22:30:24 +0800

    k

--- test.php
+++ test.php
@@ -7,14 +7,14 @@
  
 
    $report_title = "Report";
-   echo $_GET["y"];
-   echo "<br>-------------------------<br>";
     $items=  array();
-    $rows = $healthcards->find(array("y"=>(int)$_GET["y"]));
-    $rows2 = iterator_to_array($rows);
+    if(isset($_GET["y"])){
+      $rows = $healthcards->find(array("y"=>(int)$_GET["y"]));
+    }
+    
+    
 
-   // echo "-=-------------------";
-    //print_r($rows2);
+  $rows2 = iterator_to_array($rows);
     foreach ($rows2 as $key => $row) {
         $new_row = array($row['hc_lastname'].", ".$row['hc_firstname'],$row['hc_firstname'],$row['hc_position'],$row['hc_job_category'],$row['hc_business_employment']);
         array_push($items,$new_row);  
