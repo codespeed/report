b90a226f89cbd9560138151f78084af5ff361248
codespeed22 <maghanoycm@gmail.com>
2017-04-01 23:53:23 +0800

    k

--- yearly.php
+++ yearly.php
@@ -183,22 +183,14 @@ $pdf->Ln(3);
 $pdf->SetTextColor(0);
 $pdf->Cell(5, 8, 'Total Records: '.count($items), 0, 0, 'L', true);
 $pdf->SetTextColor(0,115,183);
-$pdf->Ln(8);
+$pdf->Ln(15);
 
 $pdf->SetTextColor(0);
-$pdf->Cell(40, 8, '', 0, 0, 'L', true);
-$pdf->Cell(40, 8, '', 0, 0, 'L', true);
-$pdf->Cell(40, 8, '', 0, 0, 'L', true);
-$pdf->Cell(40, 8, '', 0, 0, 'L', true);
 $pdf->Cell(90, 8, 'Josephine J. Villafuerte, MD, MCH', 0, 0, 'L', true);
 $pdf->SetTextColor(0,115,183);
 $pdf->Ln(6);
 
 $pdf->SetTextColor(0);
-$pdf->Cell(40, 8, '', 0, 0, 'L', true);
-$pdf->Cell(40, 8, '', 0, 0, 'L', true);
-$pdf->Cell(40, 8, '', 0, 0, 'L', true);
-$pdf->Cell(40, 8, '', 0, 0, 'L', true);
 $pdf->Cell(90, 8, 'City Health Officer', 0, 0, 'L', true);
 $pdf->SetFont('','B');
 $pdf->SetTextColor(0,115,183);
