737c1422b813064cbaed6186687b9d0c3523beef
codespeed22 <maghanoycm@gmail.com>
2017-04-01 23:49:12 +0800

    k

--- yearly.php
+++ yearly.php
@@ -189,7 +189,7 @@ $pdf->SetTextColor(0);
 $pdf->Cell(90, 8, 'Josephine J. Villafuerte, MD, MCH', 0, 0, 'L', true);
 
 $pdf->SetTextColor(0,115,183);
-$pdf->Ln(4);
+$pdf->Ln(6);
 
 $pdf->SetTextColor(0);
 $pdf->Cell(90, 8, 'City Health Officer', 0, 0, 'L', true);
