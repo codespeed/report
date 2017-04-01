e6eee0e0d1c05c60d515c818ed687307fc814c77
codespeed22 <maghanoycm@gmail.com>
2017-04-01 21:50:29 +0800

    k

--- test.php
+++ test.php
@@ -10,9 +10,8 @@
     $items=  array();
     $rows = $healthcards->find();
 
-
     print_r($rows);
-
+    
    /* foreach ($rows as $row) {
          $new_row = array($row['hc_lastname'].", ".$row['hc_firstname'],$row['hc_firstname'],$row['hc_position'],$row['hc_job_category'],$row['hc_business_employment']);
         array_push($items,$new_row);  
