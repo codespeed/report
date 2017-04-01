5431a29ab90c2648da9501076de26822b1112323
codespeed22 <maghanoycm@gmail.com>
2017-04-01 22:22:14 +0800

    k

--- test.php
+++ test.php
@@ -18,10 +18,12 @@
     foreach ($rows2 as $key => $val) {
          //$new_row = array($row['hc_lastname'].", ".$row['hc_firstname'],$row['hc_firstname'],$row['hc_position'],$row['hc_job_category'],$row['hc_business_employment']);
         //array_push($items,$new_row);  
-        foreach ($val as $key2 => $val2) {
-            echo $val2["hid"];
 
-        }
+      echo $val['hid'];
+        /*foreach ($val as $key2 => $val2) {
+            
+
+        }*/
      }
 
 ?>
