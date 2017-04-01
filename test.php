45e3b6da7c3276536e0d4a4a98907e74fb425581
codespeed22 <maghanoycm@gmail.com>
2017-04-01 21:55:41 +0800

    k

--- test.php
+++ test.php
@@ -3,18 +3,19 @@
 
     $mlab = new MongoClient("mongodb://eproseso:eproseso@ds059682.mlab.com:59682/eproseso");
    $db = $mlab->eproseso;
-   $applications = $db->applications;
+   $healthcards = $db->healthcards;
  
 
    $report_title = "Report";
     $items=  array();
-    $rows = $applications->find();
+    $rows = $healthcards->find();
 
-    print_r($rows);
+    //print_r($rows);
 
-   /* foreach ($rows as $row) {
-         $new_row = array($row['hc_lastname'].", ".$row['hc_firstname'],$row['hc_firstname'],$row['hc_position'],$row['hc_job_category'],$row['hc_business_employment']);
-        array_push($items,$new_row);  
-     }*/
+    foreach ($rows as $row) {
+         //$new_row = array($row['hc_lastname'].", ".$row['hc_firstname'],$row['hc_firstname'],$row['hc_position'],$row['hc_job_category'],$row['hc_business_employment']);
+        //array_push($items,$new_row);  
+      echo $row['hc_lastname'];
+     }
 
 ?>
